008ec26ff9103cd5b45368ccca84cf442b9a3e86
tommyldunn <tommylogandunn@gmail.com>
2017-09-14 12:14:15 -0400

    changed default colors

--- dev.js
+++ dev.js
@@ -1,68 +1,53 @@
-function injectGitFileStatus()
-{
+function injectGitFileStatus() {
     const timeout = 5000;
-    const addedColor = "#32CD32";
-    const modifiedColor = "#FF8C00";
+    const addedColor = "#73c990";
+    const modifiedColor = "#e2c08d";
     const ignoredOpacity = "0.4";
 
     const explorer = document.getElementById("workbench.view.explorer");
-    if (explorer)
-    {
+    if (explorer) {
         const foldersView = explorer.getElementsByClassName("explorer-folders-view")[0];
-        if (foldersView)
-        {
+        if (foldersView) {
             const tree = foldersView.getElementsByClassName("monaco-tree")[0];
-            if (tree)
-            {
+            if (tree) {
                 const firstRow = tree.getElementsByClassName("monaco-tree-row")[0];
-                if (firstRow)
-                {
+                if (firstRow) {
                     const explorerItem = firstRow.getElementsByClassName("explorer-item")[0];
-                    if (explorerItem)
-                    {
+                    if (explorerItem) {
                         const path = require("path");
                         const exec = require("child_process").exec;
 
                         const style = document.createElement("style");
                         document.body.appendChild(style);
 
-                        const resolveHome = (filepath) =>
-                        {
-                            if (filepath[0] === "~")
-                            {
+                        const resolveHome = (filepath) => {
+                            if (filepath[0] === "~") {
                                 return path.join(process.env.HOME, filepath.slice(1));
                             }
 
                             return filepath;
                         }
 
-                        const unresolveHome = (filepath) =>
-                        {
+                        const unresolveHome = (filepath) => {
                             const home = process.env.HOME;
-                            if (home && filepath.startsWith(home))
-                            {
+                            if (home && filepath.startsWith(home)) {
                                 const regex = new RegExp(`^${home}`);
                                 return filepath.replace(regex, "~");
-                            }
-                            else
-                            {
+                            } else {
                                 return filepath;
                             }
                         }
 
-                        const normalizePath = (name) =>
-                        {
+                        const normalizePath = (name) => {
                             return path.normalize(name.substr(3)).replace(/\\+$/, "").replace(/\/+$/, "").replace(/\\/g, "\\\\");
                         };
-                        
-                        const getAllSubdirectories = (name) =>
-                        {
+
+                        const getAllSubdirectories = (name) => {
                             let i = 1;
                             const paths = [];
                             const sep = path.sep.replace(/\\/g, "\\\\");
 
-                            while (i = name.indexOf(sep, i) + 1)
-                            {
+                            while (i = name.indexOf(sep, i) + 1) {
                                 paths.push(name.substr(0, i - 1));
                             }
 
@@ -71,8 +56,7 @@ function injectGitFileStatus()
 
                         const classPath = "#workbench\\.view\\.explorer .explorer-folders-view .monaco-tree .monaco-tree-rows .monaco-tree-row .explorer-item";
 
-                        const getCssEntry = (root, file, cssEntry) =>
-                        {
+                        const getCssEntry = (root, file, cssEntry) => {
                             const filepath = unresolveHome(path.join(root, file).replace(/\\/g, "\\\\"));
                             return `${classPath}[title="${filepath}" i]{${cssEntry}}`;
                         }
@@ -80,73 +64,76 @@ function injectGitFileStatus()
                         const firstFileDir = path.normalize(path.dirname(explorerItem.getAttribute("title")));
 
                         const gitRootCommand = "git rev-parse --show-toplevel";
-                        const gitRootOptions = { cwd: resolveHome(firstFileDir) };
-                        const gitRootCallback = (error, stdout, stderr) =>
-                        {
-                            if (!error)
-                            {
+                        const gitRootOptions = {
+                            cwd: resolveHome(firstFileDir)
+                        };
+                        const gitRootCallback = (error, stdout, stderr) => {
+                            if (!error) {
                                 const gitRoot = stdout.trim();
-                                const startGitStatusChecks = () =>
-                                {
+                                const startGitStatusChecks = () => {
                                     // run git status
                                     const gitStatusCommand = "git status --short --ignored";
-                                    const gitStatusOptions = { cwd: resolveHome(gitRoot) }
-                                    const gitStatusCallback = (error, stdout, stderr) =>
-                                    {
-                                        if (!error)
-                                        {
+                                    const gitStatusOptions = {
+                                        cwd: resolveHome(gitRoot)
+                                    }
+                                    const gitStatusCallback = (error, stdout, stderr) => {
+                                        if (!error) {
                                             const files = stdout.split("\n");
 
-                                            const added = files.filter(name => { return name.startsWith("?? "); }).map(name => { return normalizePath(name); });
-                                            const modified = files.filter(name => { return name.startsWith(" M "); }).map(name => { return normalizePath(name); });
-                                            const ignored = files.filter(name => { return name.startsWith("!! "); }).map(name => { return normalizePath(name); });
+                                            const added = files.filter(name => {
+                                                return name.startsWith("?? ");
+                                            }).map(name => {
+                                                return normalizePath(name);
+                                            });
+                                            const modified = files.filter(name => {
+                                                return name.startsWith(" M ");
+                                            }).map(name => {
+                                                return normalizePath(name);
+                                            });
+                                            const ignored = files.filter(name => {
+                                                return name.startsWith("!! ");
+                                            }).map(name => {
+                                                return normalizePath(name);
+                                            });
 
                                             let html = "";
-    
+
                                             const addedFolders = new Set();
                                             const modifiedFolders = new Set();
 
                                             // files
-                                            added.forEach(addedFile =>
-                                            {
+                                            added.forEach(addedFile => {
                                                 const subdirectories = getAllSubdirectories(addedFile);
-                                                subdirectories.forEach(subdirectory =>
-                                                {
+                                                subdirectories.forEach(subdirectory => {
                                                     addedFolders.add(subdirectory);
                                                 });
 
                                                 html += getCssEntry(gitRoot, addedFile, `color:${addedColor};`);
                                             });
 
-                                            modified.forEach(modifiedFile =>
-                                            {
+                                            modified.forEach(modifiedFile => {
                                                 const subdirectories = getAllSubdirectories(modifiedFile);
-                                                subdirectories.forEach(subdirectory =>
-                                                {
+                                                subdirectories.forEach(subdirectory => {
                                                     modifiedFolders.add(subdirectory);
                                                 });
 
                                                 html += getCssEntry(gitRoot, modifiedFile, `color:${modifiedColor};`);
                                             });
 
-                                            ignored.forEach(ignoredFile =>
-                                            {
+                                            ignored.forEach(ignoredFile => {
                                                 html += getCssEntry(gitRoot, ignoredFile, `opacity:${ignoredOpacity};`);
                                             });
 
                                             // folders
-                                            addedFolders.forEach((addedFolder) =>
-                                            {
+                                            addedFolders.forEach((addedFolder) => {
                                                 html += getCssEntry(gitRoot, addedFolder, `color:${addedColor};`);
                                             });
 
-                                            modifiedFolders.forEach((modifiedFolder) =>
-                                            {
+                                            modifiedFolders.forEach((modifiedFolder) => {
                                                 html += getCssEntry(gitRoot, modifiedFolder, `color:${modifiedColor};`);
                                             });
 
-                                            if (style.innerHTML !== html)
-                                            {
+                                            if (style.innerHTML !== html) {
                                                 style.innerHTML = html;
                                             }
                                         }
--- gulpfile.js
+++ gulpfile.js
@@ -10,87 +10,71 @@ const minify = composer(uglifyjs, console);
 const gutil = require('gulp-util');
 const lines = require('read-last-lines');
 
-function get_workbench_path(callback)
-{
-    const resourcesPaths = [
-        "C:\\Program Files\\Microsoft VS Code\\resources", // win64
-        "C:\\Program Files (x86)\\Microsoft VS Code\\resources", //win32
-        "/Applications/Visual Studio Code.app/Contents/Resources", // mac
-        "/usr/share/code/resources", // linux
-        "/opt/visual-studio-code/resources/" // arch linux
-    ];
+function get_workbench_path(callback) {
+  const resourcesPaths = [
+    "C:\\Program Files\\Microsoft VS Code\\resources", // win64
+    "C:\\Program Files (x86)\\Microsoft VS Code\\resources", //win32
+    "/Applications/Visual Studio Code.app/Contents/Resources", // mac
+    "/usr/share/code/resources", // linux
+    "/opt/visual-studio-code/resources/" // arch linux
+  ];
 
-    const potentials = [];
-    resourcesPaths.forEach(x =>
-    {
-        potentials.push(path.join(x, "/app/out/vs/workbench/workbench.main.js"));
-    });
+  const potentials = [];
+  resourcesPaths.forEach(x => {
+    potentials.push(path.join(x, "/app/out/vs/workbench/workbench.main.js"));
+  });
 
-    const processing = potentials.slice();
-    let found = false;
+  const processing = potentials.slice();
+  let found = false;
 
-    const accessed = (err, workbench) =>
-    {
-        if (!found)
-        {
-            if (!err)
-            {
-                found = true;
-                lines.read(workbench, 1).then((line) =>
-                {
-                    if (line.startsWith("function injectGitFileStatus()"))
-                    {
-                        callback(new Error("vscode-explorer-git-status is already installed."), null);
-                    }
-                    else
-                    {
-                        callback(err, workbench);
-                    }
-                });
-            }
-            else
-            {
-                processing.pop();
-                if (processing.length == 0)
-                {
-                    found = true;
-                    callback(new Error("Could not locate workbench.main.js or you do not have sufficient permissions.", null));
-                }
-            }
+  const accessed = (err, workbench) => {
+    if (!found) {
+      if (!err) {
+        found = true;
+        lines.read(workbench, 1).then((line) => {
+          if (line.startsWith("function injectGitFileStatus()")) {
+            callback(new Error("vscode-explorer-git-status is already installed."), null);
+          } else {
+            callback(err, workbench);
+          }
+        });
+      } else {
+        processing.pop();
+        if (processing.length == 0) {
+          found = true;
+          callback(new Error("Could not locate workbench.main.js or you do not have sufficient permissions.", null));
         }
-    };
+      }
+    }
+  };
 
-    potentials.forEach(x =>
-    {
-        fs.access(x, fs.constants.R_OK | fs.constants.W_OK, (err) => accessed(err, x));
-    });
+  potentials.forEach(x => {
+    fs.access(x, fs.constants.R_OK | fs.constants.W_OK, (err) => accessed(err, x));
+  });
 }
 
-gulp.task("minify", (callback) =>
-{
-    pump([ gulp.src("dev.js"), minify(), rename("dist.min.js"), gulp.dest("./") ], callback);
+gulp.task("minify", (callback) => {
+  pump([
+    gulp.src("dev.js"), minify(), rename("dist.min.js"), gulp.dest("./")
+  ], callback);
 });
 
-gulp.task("install", ["minify"], (callback) =>
-{
-    get_workbench_path((err, workbench) =>
-    {
-        if (!err && workbench)
-        {
-            gutil.log("Installing to " + workbench);
+gulp.task("install", ["minify"], (callback) => {
+  get_workbench_path((err, workbench) => {
+    if (!err && workbench) {
+      gutil.log("Installing to " + workbench);
 
-            const code = fs.readFileSync("dist.min.js");
-            pump([ gulp.src(workbench),
-                    rename("workbench.main.js." + Date.now()),
-                    gulp.dest(path.dirname(workbench)),
-                    rename("workbench.main.js"),
-                    insert.append(code),
-                    gulp.dest(path.dirname(workbench))
-            ], callback);
-        }
-        else
-        {
-            callback(err);
-        }
-    });
+      const code = fs.readFileSync("dist.min.js");
+      pump([
+        gulp.src(workbench),
+        rename("workbench.main.js." + Date.now()),
+        gulp.dest(path.dirname(workbench)),
+        rename("workbench.main.js"),
+        insert.append(code),
+        gulp.dest(path.dirname(workbench))
+      ], callback);
+    } else {
+      callback(err);
+    }
+  });
 });
